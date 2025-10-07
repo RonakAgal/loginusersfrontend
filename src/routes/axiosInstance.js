14d34a01c9dc55cc33e1b7baa5714b3e5b755d35
RonakAgal <144449528+RonakAgal@users.noreply.github.com>
2025-10-07 23:57:06 +0530

    Update axiosInstance.js

--- src/routes/axiosInstance.js
+++ src/routes/axiosInstance.js
@@ -5,3 +5,4 @@ export const AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials:true
 })
+console.log("BASE URL:", import.meta.env.VITE_API_BASE_URL);

8c2fb69c2cf680df5d601733c0b855133f2dd48b
Ronak Agal <ronakagal123@gmail.com>
2025-10-08 12:18:07 +0530

    some changes

--- src/routes/axiosInstance.js
+++ src/routes/axiosInstance.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 export const AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
-    withCredentials:true
+    withCredentials:true,
 })
-console.log("BASE URL:", import.meta.env.VITE_API_BASE_URL);
+
+ 
